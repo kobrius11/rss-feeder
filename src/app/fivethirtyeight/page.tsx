fa3bc3e673ab2d12331f18898a26c07c441c070b
kobrius11 <kobrius11@gmail.com>
2025-01-28 22:53:44 +0200

    well it builds i guess

--- src/app/fivethirtyeight/page.tsx
+++ src/app/fivethirtyeight/page.tsx
@@ -1,11 +1,11 @@
 import MainContainer from "@/app/components/containers/MainContainer";
-import FiveThrityEightRssFeed from "./RssFeed";
+// import FiveThrityEightRssFeed from "./RssFeed";
 
 
 export default function FiveThirtyEightPage() {
   return (
     <MainContainer>
-      <FiveThrityEightRssFeed />
+      {/* <FiveThrityEightRssFeed /> */}
     </MainContainer>
   );
 }
